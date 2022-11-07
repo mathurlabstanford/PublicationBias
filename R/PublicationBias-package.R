facf04a512f285792b5bc40200e946949380731b
Mika Braginsky <mika.br@gmail.com>
2022-11-07 17:04:26 -0500

    add pkg docs

--- R/PublicationBias-package.R
+++ R/PublicationBias-package.R
@@ -1,5 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import stats
 #' @import ggplot2
 #' @importFrom dplyr %>%
 #' @importFrom rlang .data
+## usethis namespace: end
 NULL
