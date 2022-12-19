803372c68cc81770eb04499a931497d1c788ce01
Mika Braginsky <mika.br@gmail.com>
2022-12-19 16:52:31 -0500

    fix stats imports to remove warning

--- R/PublicationBias-package.R
+++ R/PublicationBias-package.R
@@ -4,11 +4,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @import stats
 #' @import ggplot2
 #' @importFrom dplyr filter group_by if_else mutate summarise tibble
 #' @importFrom glue glue
 #' @importFrom Rdpack reprompt
 #' @importFrom rlang .data
+#' @importFrom stats optimize pnorm pt qnorm qt
 ## usethis namespace: end
 NULL
